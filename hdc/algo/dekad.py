ef310f984a857b2fd3561dc59bd2ccdd0b9e24f1
Kirill Kouzoubov <kirill888@gmail.com>
2023-10-05 17:46:07 +1100

    Add doc comments to Dekad members

--- hdc/algo/dekad.py
+++ hdc/algo/dekad.py
@@ -17,9 +17,7 @@ class Dekad:
     __slots__ = ("_dkd",)
 
     def __init__(self, dekad: Union[str, int, datetime, date]):
-        """
-        Format ``YYYYMMd{1,2,3}``.
-        """
+        """Format ``YYYYMMd{1,2,3}``."""
         if isinstance(dekad, str):
             year, month, idx = int(dekad[:4]), int(dekad[4:6]), int(dekad[-1])
             assert 1 <= month <= 12
@@ -33,14 +31,17 @@ class Dekad:
 
     @property
     def year(self) -> int:
+        """Extract year."""
         return self._dkd // 36
 
     @property
     def month(self) -> int:
+        """Extract month."""
         return 1 + (self._dkd % 36) // 3
 
     @property
     def day(self) -> int:
+        """Extract day."""
         return 1 + (self._dkd % 3) * 10
 
     @property
@@ -50,18 +51,23 @@ class Dekad:
 
     @property
     def raw(self) -> int:
+        """Access raw integer representations."""
         return self._dkd
 
     def __str__(self):
+        """Return string representation."""
         return f"{self.year:04d}{self.month:02d}d{self.idx}"
 
     def __repr__(self):
+        """Return string representation."""
         return f'Dekad("{str(self)}")'
 
     def __hash__(self):
+        """Return Hash."""
         return hash(self._dkd)
 
     def __eq__(self, other: object) -> bool:
+        """Check equality with other Dekad or a string."""
         if isinstance(other, str):
             return str(self) == other
         if isinstance(other, Dekad):
@@ -70,36 +76,42 @@ class Dekad:
 
     @property
     def start_date(self) -> datetime:
+        """Start date as python ``datetime``."""
         return datetime(self.year, self.month, self.day)
 
     @property
     def end_date(self) -> datetime:
+        """End date as python ``datetime``."""
         return (self + 1).start_date - timedelta(microseconds=1)
 
     @property
     def date_range(self) -> Tuple[datetime, datetime]:
+        """Start and end dates as python ``datetime``."""
         return self.start_date, self.end_date
 
     @property
     def ndays(self) -> int:
-        """Number of days in dekad"""
+        """Number of days in dekad."""
         return (self.end_date - self.start_date + timedelta(microseconds=1)).days
 
     def __radd__(self, n: int) -> "Dekad":
+        """Addition with integer."""
         return Dekad(self._dkd + n)
 
     def __add__(self, n: int) -> "Dekad":
+        """Addition with integer."""
         return Dekad(self._dkd + n)
 
     @overload
     def __sub__(self, other: int) -> "Dekad":
-        ...
+        """Subtraction with integer."""
 
     @overload
     def __sub__(self, other: "Dekad") -> int:
-        ...
+        """Subtraction with Dekad."""
 
     def __sub__(self, other: Union[int, "Dekad"]) -> Union["Dekad", int]:
+        """Subtraction with integer|Dekad."""
         if isinstance(other, int):
             return Dekad(self._dkd - other)
         return self._dkd - other._dkd
